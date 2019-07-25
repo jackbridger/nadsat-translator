d4785ae378932b19ea3d63a4d185577d9be1551a
Colette <colette.rs.shaw@gmail.com>
2019-07-25 12:35:31 +0100

    write function that searches dictionary object for keys that contain a string and pushes those keys to an array

--- src/handler.js
+++ src/handler.js
@@ -2,19 +2,38 @@
 
 // https://github.com/sofer/sssk/blob/master/handlers.js
 
-var handlers = (module.exports = {});
-var data = require("./model");
+// var handlers = (module.exports = {});
+// var data = require("./model");
+//
+// var headers = {
+//   "content-type": "text/html"
+// };
+//
+// handlers.home = function(req, res) {
+//   res.writeHead(200, headers);
+//   res.end(data.greeting);
+// };
+//
+// handlers.notFound = function(req, res) {
+//   res.writeHead(404, headers);
+//   res.end("Resource not found");
+// };
 
-var headers = {
-  "content-type": "text/html"
-};
+// autocomplete section
+// user types into text box; on each key press, search dictionary object and return possible words
+// (add event listener etc.)
+var inputString = "test";
+var testObject = { tested: "1", testing: "2", no: "3" };
+var returnedWords = [];
 
-handlers.home = function(req, res) {
-  res.writeHead(200, headers);
-  res.end(data.greeting);
-};
+function pushKeys(key) {
+  var keyString = key.toString();
+  if (keyString.includes(inputString)) {
+    returnedWords.push(key);
+  }
+}
 
-handlers.notFound = function(req, res) {
-  res.writeHead(404, headers);
-  res.end("Resource not found");
-};
+Object.keys(testObject).forEach(pushKeys);
+
+console.log(returnedWords);
+// display possible words
