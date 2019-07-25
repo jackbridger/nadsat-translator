ad01e19187f6250f83c10770c20c91f3aa791deb
georgia.shaw <georgia@cranbrookw4.com>
2019-07-25 17:09:43 +0100

    set up path for public on server

--- src/router.js
+++ src/router.js
@@ -7,7 +7,8 @@ var handlers = require("./handler");
 var routes = {
   "/": handlers.home,
   "404": handlers.notFound,
-  "translate": handlers.translate
+  "translate": handlers.translate,
+  "public":handlers.public
 };
 
 // /translate#q=good
@@ -15,10 +16,14 @@ var routes = {
 module.exports = function(req, res) {
   if (routes[req.url]) {
     routes[req.url](req, res);
-  } 
+  }
   else if (req.url.includes('translate')) {
     routes["translate"](req, res);
   }
+  else if (req.url.includes('public')) {
+    console.log("i am in public!")
+      routes["public"](req, res);
+    }
   else {
     routes[404](req, res);
   }
