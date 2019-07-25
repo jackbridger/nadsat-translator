451b83da37c2e001b91e6d4ce7fd17b0e223b59a
georgia.shaw <georgia@cranbrookw4.com>
2019-07-25 15:17:34 +0100

    translated english word to nad sat
    > managed to (somehow) get english word which is shown in url to return its nadsat equivalent on the html

--- src/handler.js
+++ src/handler.js
@@ -50,25 +50,25 @@ handlers.translate = (req, res) => {
 // autocomplete section
 // user types into text box; on each key press, search dictionary object and return possible words
 // (add event listener etc.)
-// function getInput() {
-//   document.getElementById("inputField").value = inputString;
-// }
-// console.log(inputString);
-//
-// document.getElementById("inputField").addEventListener("onkeydown", getInput);
-// // var testObject = { tested: "1", testing: "2", no: "3" };
-// var testObject = data.nadsat;
-//
-// var returnedWords = [];
-//
-// function pushKeys(key) {
-//   var keyString = key.toString();
-//   if (keyString.includes(inputString)) {
-//     returnedWords.push(key);
-//   }
-// }
-//
-// Object.keys(testObject).forEach(pushKeys);
-//
-// console.log(returnedWords);
-// // display possible words
+function getInput() {
+  document.getElementById("inputField").value = inputString;
+}
+console.log(inputString);
+
+document.getElementById("inputField").addEventListener("onkeydown", getInput);
+// var testObject = { tested: "1", testing: "2", no: "3" };
+var testObject = data.nadsat;
+
+var returnedWords = [];
+
+function pushKeys(key) {
+  var keyString = key.toString();
+  if (keyString.includes(inputString)) {
+    returnedWords.push(key);
+  }
+}
+
+Object.keys(testObject).forEach(pushKeys);
+
+console.log(returnedWords);
+// display possible words
