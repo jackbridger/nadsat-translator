dcdcbf06880910197e3265e10e93e35c07990d12
georgia.shaw <georgia@cranbrookw4.com>
2019-07-25 15:15:21 +0100

    translated english word to nadsat managed to (somehow) get english word which is shown in url to return its nadsat equivalent on the html

--- src/handler.js
+++ src/handler.js
@@ -32,6 +32,8 @@ handlers.notFound = function(req, res) {
 };
 
 handlers.translate = (req, res) => {
+  let searchWord = req.url.split('?')[1];
+  let wordTranslation = data.nadsat[searchWord];
   const filePath = path.join(__dirname + "/.." + "/public" + "/index.html");
   fs.readFile(filePath, (error, file) => {
     if (error) {
@@ -41,32 +43,32 @@ handlers.translate = (req, res) => {
     res.writeHead(200, {
       "Content-Type": "text/html"
     });
-    res.end(req.url.split('?')[1]);
+    res.end(wordTranslation);
   });
 }
 
 // autocomplete section
 // user types into text box; on each key press, search dictionary object and return possible words
 // (add event listener etc.)
-function getInput() {
-  document.getElementById("inputField").value = inputString;
-}
-console.log(inputString);
-
-document.getElementById("inputField").addEventListener("onkeydown", getInput);
-// var testObject = { tested: "1", testing: "2", no: "3" };
-var testObject = data.nadsat;
-
-var returnedWords = [];
-
-function pushKeys(key) {
-  var keyString = key.toString();
-  if (keyString.includes(inputString)) {
-    returnedWords.push(key);
-  }
-}
-
-Object.keys(testObject).forEach(pushKeys);
-
-console.log(returnedWords);
-// display possible words
+// function getInput() {
+//   document.getElementById("inputField").value = inputString;
+// }
+// console.log(inputString);
+//
+// document.getElementById("inputField").addEventListener("onkeydown", getInput);
+// // var testObject = { tested: "1", testing: "2", no: "3" };
+// var testObject = data.nadsat;
+//
+// var returnedWords = [];
+//
+// function pushKeys(key) {
+//   var keyString = key.toString();
+//   if (keyString.includes(inputString)) {
+//     returnedWords.push(key);
+//   }
+// }
+//
+// Object.keys(testObject).forEach(pushKeys);
+//
+// console.log(returnedWords);
+// // display possible words
